41df0dc87ae1947b556d5fceb877beccf5adb9d8
Camilo <est.camiloa.trujillo@unimilitar.edu.co>
2025-05-24 16:37:14 -0600

    Detector de Cesta
    Se hizo la deteccion de la cesta al pasar por la zona de anotacion, falta hacer que la pelota colision en los bordes del aro, solo para que cuente como canasta cuando pase bien por el centro del aro

---
+++ SimulacionBasket/Assets/scripts/Jugabilidad/DetectorCanasta.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class DetectorCanasta : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Lanzador lanzador = other.GetComponent<Lanzador>();
+        if (lanzador != null && lanzador.GetVelocidad().y < 0)
+        {
+            lanzador.RegistrarPunto();
+        }
+    }
+}
